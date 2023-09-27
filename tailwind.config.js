98f15035084dd99583fa60095ed7c6537a99a0a3
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-09-27 22:35:50 +0300

    Uploading

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./src/assets/images/hero4.png')",
+        'service-pattern': "url('./src/assets/images/service.jpg')",
+      },
+      screens: {
+        sm: '576px',
+        md: '768px',
+        lg: '922px',
+        xl: '1200px',
+        '2xl': '1400px'
+      },
+      boxShadow: {
+        '3xl': '10px 10px 20px #00000014',
+      },
+    },
+  },
+  plugins: [],
+}
